1ff44d2d51352c9fdb2f110abcf6757e3f711a71
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-22 20:49:32 -0600

    5 files modified, 4 added, 3 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import 'zone.js';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <h2>Binding Date Objects to the HTML datepicker in Angular</h2>
 
-import { AppModule } from './app/app.module';
+    <label for="date-string">Regular string value</label>
+    <input [formControl]="dateStringControl" type="date" id="date-string" />
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+
+    <label for="date-object">Date object value with directive help</label>
+    <input [formControl]="dateObjectControl" type="date" id="date-object" />
+
+
+    <label for="date">Native date object value</label>
+    <input [valueAsDate]="date" (change)="updateDate($event)" type="date" id="date" />
+
+
+    <h3>Values</h3>
+
+    <p>String Value:<br> {{dateStringControl.value}}</p>
+    <p>Date Object:<br> {{dateObjectControl.value}}</p>
+    <p>Date (Native):<br> {{date}}</p>
+  `,
+})
+export class App {
+  date = new Date();
+  dateStringControl = new FormControl('2020-09-28');
+  dateObjectControl = new FormControl(new Date());
+
+  updateDate(event: any) {
+    this.date = event.target.valueAsDate;
   }
-  window['ngRef'] = ref;
+}
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+bootstrapApplication(App);
